dbfccff3ddb18331f73259a10cbcc2b2f213a838
Vincent Jousse <vincent@jousse.org>
2022-02-04 20:37:21 +0100

    Render blog post

--- src/content.rs
+++ src/content.rs
@@ -5,6 +5,8 @@ use pulldown_cmark::{html, Options, Parser};
 use serde::Deserialize;
 use std::error::Error;
 use std::fs;
+use std::fs::File;
+use std::io::Write;
 use tera::{Context, Tera};
 use walkdir::WalkDir;
 
@@ -19,7 +21,7 @@ lazy_static! {
                 ::std::process::exit(1);
             }
         };
-        tera.autoescape_on(vec!["html", ".sql"]);
+        tera.autoescape_on(vec![]);
         tera
     };
 }
@@ -51,9 +53,9 @@ pub fn convert_md_to_html(md_content: &str) -> String {
     html_output
 }
 
-pub fn render_template(context: Context, template_path: &str) {
+pub fn render_template_to_html(context: Context, template_path: &str) -> Option<String> {
     match TEMPLATES.render(template_path, &context) {
-        Ok(s) => println!("{:?}", s),
+        Ok(s) => Some(s),
         Err(e) => {
             println!("Error: {}", e);
             let mut cause = e.source();
@@ -61,8 +63,9 @@ pub fn render_template(context: Context, template_path: &str) {
                 println!("Reason: {}", e);
                 cause = e.source();
             }
+            None
         }
-    };
+    }
 }
 
 pub fn get_files_for_directory(directory: &str) -> Vec<FilePath> {
@@ -75,8 +78,10 @@ pub fn get_files_for_directory(directory: &str) -> Vec<FilePath> {
 }
 
 pub fn list_directory(directory: &str) {
+    // Get the list of files
     let files_to_parse: Vec<FilePath> = get_files_for_directory(directory);
 
+    // Read content of every file, get the front_matter content
     let posts_contents: Vec<PostContent> = files_to_parse
         .into_iter()
         .filter_map(|file_path| parse_file(&file_path))
@@ -84,14 +89,26 @@ pub fn list_directory(directory: &str) {
 
     for post in &posts_contents {
         let html_content = convert_md_to_html(&post.content);
-        log::info!("{}", html_content);
+        //log::info!("{}", html_content);
 
         let mut context = Context::new();
-        context.insert("post_content", &html_content);
-        render_template(context, "blog/post.html");
+
+        if let Some(front_matter) = &post.front_matter {
+            log::info!("{:?}", &front_matter.title);
+            context.insert("title", &front_matter.title);
+            context.insert("date", &front_matter.date);
+
+            context.insert("post_content", &html_content);
+
+            if let Some(html) = render_template_to_html(context, "blog/post.html") {
+                let mut f = File::create(format!("output/{}.html", &front_matter.slug))
+                    .expect("Unable to create file");
+                f.write_all(html.as_bytes()).expect("Unable to write data");
+            };
+        };
     }
 
-    // log::info!("{:?}", posts_contents);
+    //log::info!("{:?}", posts_contents);
 }
 
 pub fn parse_file(file_path: &str) -> Option<PostContent> {
