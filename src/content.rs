d71eca05777411e3cb4fc15460b9377afd287e79
Vincent Jousse <vincent@jousse.org>
2022-02-27 20:23:49 +0000

    Remove useless comments

--- src/content.rs
+++ src/content.rs
@@ -25,13 +25,6 @@ mod custom_date_format {
 
     const FORMAT: &str = "%Y-%m-%d %H:%M:%S%z";
 
-    // The signature of a serialize_with function must follow the pattern:
-    //
-    //    fn serialize<S>(&T, S) -> Result<S::Ok, S::Error>
-    //    where
-    //        S: Serializer
-    //
-    // although it may also be generic over the input types T.
     pub fn serialize<S>(date: &DateTime<FixedOffset>, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: Serializer,
@@ -40,13 +33,6 @@ mod custom_date_format {
         serializer.serialize_str(&s)
     }
 
-    // The signature of a deserialize_with function must follow the pattern:
-    //
-    //    fn deserialize<'de, D>(D) -> Result<T, D::Error>
-    //    where
-    //        D: Deserializer<'de>
-    //
-    // although it may also be generic over the output types T.
     pub fn deserialize<'de, D>(deserializer: D) -> Result<DateTime<FixedOffset>, D::Error>
     where
         D: Deserializer<'de>,
