f7d433323d0856611cbdf0a42a14c5fbf633e831
Vincent Jousse <vincent@jousse.org>
2024-10-06 22:55:43 +0200

    fix: kill processes when exiting dev script

--- dev.sh
+++ dev.sh
@@ -1,2 +1,5 @@
+#!/usr/bin/env bash
+
+trap "trap - SIGTERM && kill -- -$$" SIGINT SIGTERM EXIT
 simple-http-server -i output &
-watchexec -e md,html,rs,css cargo run -- -p &
+watchexec -e md,html,rs,css cargo run -- -p
